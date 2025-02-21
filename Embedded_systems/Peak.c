0819418d4090368447f1d5b09ca8e4c03bfe328a
Abdullah-Ahmedd <163368044+Abdullah-Ahmedd@users.noreply.github.com>
2025-02-21 22:06:31 +0200

    Update Peak.c

--- Embedded_systems/Peak.c
+++ Embedded_systems/Peak.c
@@ -4,7 +4,15 @@ void findpeak(int arr[],int size)
 {
 if(size==1)
 cout<<"the size of this array is only 1 so the peak is:"<<arr[0];
-
+if(arr[size-1]>=arr[size-2])
+cout<<"the last element is the peak"<<arr[size-1];
+for(int i=1;i<size-1;i++)
+{
+    if(arr[i-1]<=arr[i] && arr[i]>=arr[i+1])
+{
+    cout<<"there is a peak at index "<<i<<" its value it: "<<arr[i]<<endl;
+}
+}
 }
 int main()
 {
@@ -14,7 +22,9 @@ int temp;
 cout<<"enter the array size you want :";
 cin>>x;
 for(int i=0;i<x;i++)
-{cin>>temp;
+{cout<<"please enter the element number "<<i<<":";
+    cin>>temp;
 arr[i]=temp;}
-findpeak(arr,x)
+findpeak(arr,x);
+return 0;
 }
